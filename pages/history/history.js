bfa8561f34e8b2a8c7796634df5ea8330aff123c
lgguan <lgguan@live.cn>
2022-09-26 22:30:20 +0800

    '添加补卡功能'

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,139 +1,231 @@
 // pages/history/history.js
 const app = getApp()
 const { formatDate } = require("../../utils/util");
+const typeMap = {
+    elementaryRank: 'elementary',
+    middleRank: 'intermediate',
+    highRank: 'superior',
+}
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    endDate: '',
-    date: '',
-    elementaryRank: [],
-    middleRank: [],
-    highRank: [],
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        endDate: '',
+        date: '',
+        elementaryClockIn: true,
+        elementaryRank: [],
+        middleClockIn: true,
+        middleRank: [],
+        highClockIn: true,
+        highRank: [],
+        clockinObj: {},
+        clockinIds: {},
+        saving: false,
+    },
 
-  onDateChange({ detail: { value } }) {
-    this.setData({
-      date: value
-    })
-  },
-
-  onCheckboxChange({ currentTarget: { id } }) {
-    const preceptList = this.data[id]
-    this.setData({
-      [id]: preceptList
-    })
-  },
-
-  fetchHistory(date) {
-    const time = new Date(date).valueOf()
-    wx.showLoading({
-      title: '加载打卡数据中',
-    })
-    wx.request({
-      url: `${app.globalData.baseUrl}/api/clockin/history`,
-      method: 'POST',
-      header: {
-        'content-type': 'application/x-www-form-urlencoded',
-        token: app.globalData.userInfo.token,
-      },
-      data: {
-        time
-      },
-      success: (res) => {
-        if (res.statusCode === 200) {
-          const { elementaryRank, middleRank, highRank, highRanktitle } = res.data
-          this.setData({
-            elementaryRank,
-            middleRank,
-            highRank,
-            highRanktitle
-          })
-          wx.hideLoading()
-        } else {
-          wx.hideLoading({
-            success: (res) => {
-              wx.showLoading({
-                title: '数据加载失败，请稍后重试',
-                icons: 'error'
-              })
-            },
-          })
-        }
-      },
-      fail: (err) => {
-        wx.hideLoading({
-          success: (res) => {
-            wx.showLoading({
-              title: '数据加载失败，请稍后重试',
-              icons: 'error'
-            })
-          },
+    onDateChange({ detail: { value } }) {
+        this.setData({
+            date: value
         })
-      },
-      complete: () => {
-        wx.stopPullDownRefresh()
-      }
-    })
-  },
+    },
 
-  watchDate(date) {
-    this.fetchHistory(date)
-  },
+    onCheckboxChange({ currentTarget: { id }, detail: { value } }) {
+        const preceptList = this.data[id]
+        const clockined = this.data.clockinObj[id]
+        const clockinIdKey = `clockinIds.${id}`
+        if (!clockined) {
+            preceptList.forEach(item => {
+                item.isClockin = value.indexOf(item.preceptId + "") !== -1
+            })
+        }
+        console.log(clockined, value, preceptList);
+        this.setData({
+            [id]: preceptList,
+            [clockinIdKey]: value
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-    app.watch(this.data, 'date', (date) => this.watchDate(date))
-    const date = formatDate(new Date())
-    this.setData({
-      date,
-      endDate: date
-    })
-  },
+    onSave({ currentTarget: { id } }) {
+        const { saving } = this.data
+        const clockined = this.data.clockinObj[id]
+        const clockinIds = this.data.clockinIds[id]
+        console.log(id, saving, clockined);
+        if (clockined || saving || clockinIds.length === 0) {
+            return
+        }
+        wx.showLoading({
+            title: '正在补卡',
+        })
+        this.setData({
+            saving: true
+        })
+        const time = new Date(formatDate(new Date())).valueOf()
+        wx.request({
+            url: `${app.globalData.baseUrl}/api/clockin/${typeMap[id]}`,
+            method: 'POST',
+            header: {
+                'content-type': 'application/x-www-form-urlencoded',
+                token: app.globalData.userInfo.token
+            },
+            data: {
+                time: time,
+                preceptIds: clockinIds.join()
+            },
+            success: (res) => {
+                if (res.statusCode === 200) {
+                    wx.hideLoading({
+                        success: () => {
+                            wx.showToast({
+                                title: '补卡完成',
+                                success: () => {
+                                    this.fetchHistory(this.data.date)
+                                }
+                            })
+                        }
+                    })
+                } else {
+                    wx.hideLoading({
+                        success: () => {
+                            wx.showToast({
+                                title: '补卡失败',
+                            })
+                        }
+                    })
+                }
+            },
+            fail: (err) => {
+                wx.hideLoading({
+                    success: () => {
+                        wx.showToast({
+                            title: '打卡失败',
+                        })
+                    }
+                })
+            },
+            complete: () => {
+                this.setData({
+                    saving: false
+                })
+            }
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    fetchHistory(date) {
+        const time = new Date(date).valueOf()
+        wx.showLoading({
+            title: '加载打卡数据中',
+        })
+        wx.request({
+            url: `${app.globalData.baseUrl}/api/clockin/history`,
+            method: 'POST',
+            header: {
+                'content-type': 'application/x-www-form-urlencoded',
+                token: app.globalData.userInfo.token,
+            },
+            data: {
+                time
+            },
+            success: (res) => {
+                if (res.statusCode === 200) {
+                    const { elementaryClockIn, elementaryRank, middleClockIn, middleRank, highClockIn, highRank, highRanktitle } = res.data
+                    const clockinObj = {
+                        elementaryRank: elementaryClockIn,
+                        middleRank: middleClockIn,
+                        highRank: highClockIn
+                    }
+                    this.setData({
+                        elementaryRank,
+                        middleRank,
+                        highRank,
+                        highRanktitle,
+                        clockinObj,
+                        clockinIds: {}
+                    })
+                    wx.hideLoading()
+                } else {
+                    wx.hideLoading({
+                        success: (res) => {
+                            wx.showLoading({
+                                title: '数据加载失败，请稍后重试',
+                                icons: 'error'
+                            })
+                        },
+                    })
+                }
+            },
+            fail: (err) => {
+                wx.hideLoading({
+                    success: (res) => {
+                        wx.showLoading({
+                            title: '数据加载失败，请稍后重试',
+                            icons: 'error'
+                        })
+                    },
+                })
+            },
+            complete: () => {
+                wx.stopPullDownRefresh()
+            }
+        })
+    },
 
-  },
+    watchDate(date) {
+        this.fetchHistory(date)
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+        app.watch(this.data, 'date', (date) => this.watchDate(date))
+        const date = formatDate(new Date())
+        this.setData({
+            date,
+            endDate: date
+        })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-    this.fetchHistory(this.data.date)
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  }
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+        this.fetchHistory(this.data.date)
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
 })
